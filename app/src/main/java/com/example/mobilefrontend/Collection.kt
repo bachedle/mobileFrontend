3248eda65c97d21c83c73f265e3717cd0463d7c9
My Name <thebobz2770@gmail.com>
2025-05-06 17:31:23 +0700

    collection UI + hardcode test (chua co add to collection function)

--- app/src/main/java/com/example/mobilefrontend/Collection.kt
+++ app/src/main/java/com/example/mobilefrontend/Collection.kt
@@ -1,59 +1,62 @@
 package com.example.mobilefrontend
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mobilefrontend.itemCard.AdapterClass
+import com.example.mobilefrontend.itemCard.DataClass
+import com.example.mobilefrontend.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Collection.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Collection : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: AdapterClass
+    private lateinit var cardList: ArrayList<DataClass>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_collection, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_collection, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Collection.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Collection().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+
+        // Sample data
+        cardList = ArrayList()
+        repeat(8) {
+            cardList.add(
+                DataClass(
+                    R.drawable.sample_card,
+                    "Meowscarada EX",
+                    "Paldea Evolved",
+                    "Special Illustration Rare",
+                    "256/193"
+                )
+            )
+        }
+
+        adapter = AdapterClass(cardList) { selectedCard ->
+            // Navigate to CardDetails fragment when a card is clicked
+            val cardDetailsFragment = CardDetails.newInstance(
+                selectedCard.dataImage,
+                selectedCard.dataCardName,
+                selectedCard.dataCardSet,
+                selectedCard.dataCardRarity,
+                selectedCard.dataCardCode
+            )
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, cardDetailsFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        recyclerView.adapter = adapter
+
+        return view
     }
 }
